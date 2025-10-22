0016a79ec182fc13200efa879b1a96d8bcbd7cb9
Putranugroho14 <putranugroho61@gmail.com>
2025-10-22 20:31:04 +0700

    Pertemuan 3 router middleware

---
+++ Tugas/Tugas3.md
@@ -0,0 +1,16 @@
+Tugas 3
+- **Nama:** Putra Nugroho
+- **NIM:** 20230140165
+- **Kelas:** D
+
+---
+
+Dokumentasi Pengerjaan Tugas
+| Keterangan | Screenshot |
+| :---         |     :---:      |
+| POST  |   ![Hasil POST](./checkin.jpg) |
+| POST  |   ![Hasil POST](./checkinduakali.jpg) |
+| POST  |   ![Hasil POST](./checkout.jpg) |
+| POST  |   ![Hasil POST](./checkoutduakali.jpg) |
+| GET |   ![Hasil GET role karyawan](./reportsdailynoadmin.jpg) |
+| GET |   ![Hasil GET role admin](./reportsdailyadmin.jpg) |
--- pawpraktikum1_Node_20230140165/server.js
+++ pawpraktikum1_Node_20230140165/server.js
@@ -1,30 +1,28 @@
-const express = require('express');
-const cors = require('cors'); 
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = 3001; 
+const PORT = 5000
+const morgan = require("morgan");
+
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
 
 // Middleware
-app.use(cors()); 
-app.use(express.json()); 
-
-// Middleware untuk logging setiap request
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
-
-// Panggil rute buku dari file books.js
-const bookRoutes = require('./routes/books');
-
-// Gunakan rute buku
-app.use('/api/books', bookRoutes);
-
-// Rute dasar
-app.get('/', (req, res) => {
-  res.send('Home Page for API');
+app.get("/", (req, res) => {
+  res.send("Home Page for API");
 });
-
-// Jalankan server
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
 });
